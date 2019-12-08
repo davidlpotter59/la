0067539d9d9402d2adb29237ee297b534c15440c
davep@davidlpotter.com <davep@davidlpotter.com>
2019-12-08 16:37:40 -0500

    made some cosmetic changes

--- fizz.py
+++ fizz.py
@@ -1,6 +1,6 @@
 prompt = int(input("What how many values should we process? "))
 
-for x in range(1,prompt + 1):
+for x in range(1, prompt + 1):
     if x % 3 == 0 and x % 5 == 0:
         print("{0} Fizz Buzz".format(x))
     elif x % 3 == 0:
